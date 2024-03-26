21de2f4999ac04015dab63f18cbc1f5d1a310a4f
makklaud <1roman080283@mail.ru>
2024-03-26 16:20:11 +0600

    Добавьте файлы проекта.

---
+++ Quadratic Equation/QuadraticSolver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadratic_Equation
+{
+    public static class QuadraticSolver
+    {
+        public static int Solver(int a, int b, int c)
+        {
+            var discriminant = Math.Pow(b, 2) - (4 * a * c);
+
+            if (discriminant < 0)
+                return 0;
+            
+            var x1 = (-b + Math.Sqrt(discriminant)) / (2 *a );
+            var x2 = (-b - Math.Sqrt(discriminant)) / (2 *a );
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+            return 0;
+        }
+    }
+}
